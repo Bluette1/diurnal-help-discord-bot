5d503187e14e0490489041a7eafbeda763aed8dc
Bluette1 <marylene.sawyer@gmail.com>
2024-01-04 01:27:41 +0200

    Add Bot Commands, Events and Message handler

--- src/bot.js
+++ src/bot.js
@@ -1,39 +1,67 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-import { REST, Routes } from 'discord.js';
+import { Client, GatewayIntentBits } from "discord.js";
+import { REST, Routes } from "discord.js";
+import "dotenv/config";
 
 const commands = [
   {
-    name: 'ping',
-    description: 'Replies with Pong!',
+    name: "ping",
+    description: "Replies with Pong!",
   },
 ];
-const TOKEN = 'MTE4OTIzMzY3ODI3OTI2NjM4NQ.Gtp9uk.phFdJ6o5eWQTaBUkfQiDewc09DJaGHSWYxoU9k'
-const CLIENT_ID = '1189233678279266385'
+const TOKEN = process.env.TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
 
-const rest = new REST({ version: '10' }).setToken(TOKEN);
+const rest = new REST({ version: "10" }).setToken(TOKEN);
 
 try {
-  console.log('Started refreshing application (/) commands.');
+  console.log("Started refreshing application (/) commands.");
 
   await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
 
-  console.log('Successfully reloaded application (/) commands.');
+  console.log("Successfully reloaded application (/) commands.");
 } catch (error) {
   console.error(error);
 }
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
+client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
   }
 });
+
+client.on("messageCreate", async (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+
+client.on("guildMemberAdd", async (member) => {
+  console.log('yay!')
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+  if (!channel) return;
+  channel.send(`Welcome ${member}!`)
+});
+
+client.on("error", (err) => {
+  console.warn(err);
+});
+
+
 
 client.login(TOKEN);
