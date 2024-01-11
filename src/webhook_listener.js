ba7d7581645aef8d89f0e590097dd1c6857582fe
Bluette1 <marylene.sawyer@gmail.com>
2024-01-11 16:52:53 +0200

    Fix Lint

--- src/webhook_listener.js
+++ src/webhook_listener.js
@@ -8,32 +8,32 @@ const app = express();
 app.use(bodyParser.json());
 
 class WebhookListener extends EventEmitter {
-	listen() {
-		app.post('/kofi', (req, res) => {
-			const data = req.body.data;
-			const { message, timestamp } = data;
-			const amount = parseFloat(data.amount);
-			const senderName = data.from_name;
-			const paymentId = data.message_id;
-			const paymentSource = 'Ko-fi';
+  listen() {
+    app.post('/kofi', (req, res) => {
+      const data = req.body.data;
+      const { message, timestamp } = data;
+      const amount = parseFloat(data.amount);
+      const senderName = data.from_name;
+      const paymentId = data.message_id;
+      const paymentSource = 'Ko-fi';
 
-			// The OK is just for us to see in Postman. Ko-fi doesn't care
-			// about the response body, it just wants a 200.
-			res.send({ status: 'OK' });
+      // The OK is just for us to see in Postman. Ko-fi doesn't care
+      // about the response body, it just wants a 200.
+      res.send({ status: 'OK' });
 
-			this.emit(
-				'donation',
-				paymentSource,
-				paymentId,
-				timestamp,
-				amount,
-				senderName,
-				message,
-			);
-		});
+      this.emit(
+        'donation',
+        paymentSource,
+        paymentId,
+        timestamp,
+        amount,
+        senderName,
+        message,
+      );
+    });
 
-		app.listen(PORT);
-	}
+    app.listen(PORT);
+  }
 }
 
 const listener = new WebhookListener();
